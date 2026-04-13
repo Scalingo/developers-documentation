6c26071a92acd6be718b97dca15b0926a2964924
TheoPouletSouchard <theo.poulet.souchard@scalingo.com>
2026-04-13 17:57:49 +0200

    [STORY-3582] New branding (#657)
    * Change logo + favicon
    
    * Use new IBM font
    
    * Add new design system colors variables
    
    * Upgrade DocSearch + Adapt styles
    
    * Re-introduce font-size for .DockSearch
    
    * adjustments on DocSearch button styles
    
    * adjust logo height & position
    
    * Add SRI
    
    * Fix site.manifest

--- js/application.js
+++ js/application.js
@@ -1,7 +1,7 @@
 requirejs.config({
   waitSeconds: 200,
   paths: {
-    'docsearch': '//cdn.jsdelivr.net/npm/@docsearch/js@3.0.0/dist/umd/index'
+    'docsearch': '//cdn.jsdelivr.net/npm/@docsearch/js@4.6.0/dist/umd/index'
   },
   shim: {
     'search': ['docsearch'],
